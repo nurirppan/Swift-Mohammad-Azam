7626fa5b34882def4edb186bafae1535e0fcc2b2
Nur Irfan Pangestu <80233829+iosdevaltea@users.noreply.github.com>
2021-12-24 14:51:55 +0700

    + catatan webservice Webservice

--- Async:Await and Actors/1. GettingStartedAsyncAwait/GettingStartedAsyncAwait/Webservice.swift
+++ Async:Await and Actors/1. GettingStartedAsyncAwait/GettingStartedAsyncAwait/Webservice.swift
@@ -12,7 +12,7 @@ class Webservice {
     func getDate() async throws -> CurrentDate? {
         
         guard let url = URL(string: "https://ember-sparkly-rule.glitch.me/current-date") else {
-            fatalError("Url is incorrect!")
+            fatalError("Url is incorrect!") // sangat tidak di anjutkan dikarnakan akan menyebabkan crash aplikasi
         }
         
         let (data, _) = try await URLSession.shared.data(from: url)
@@ -20,3 +20,11 @@ class Webservice {
     }
     
 }
+/**
+ await hanya bisa digunakan jika menggunakan async
+ gunakan try untuk menggunakan awati
+ gunakan throws error di function setelah async
+ let (data, _) ---->>> seharusnya sepertin ini let (data, response). namun response tidak di gunakan. ganti underscore saja
+  diakhir function gunakan return try? agar mengembalikan nil data jika terjadi crash response
+ 
+ */
