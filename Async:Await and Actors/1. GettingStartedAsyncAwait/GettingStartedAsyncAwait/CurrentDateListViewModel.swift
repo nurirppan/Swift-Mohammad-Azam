444a4b59b9a694f0eb147ce96db696c82ab90543
Nur Irfan Pangestu <80233829+iosdevaltea@users.noreply.github.com>
2021-12-24 14:58:06 +0700

    + catatan view model getting started

--- Async:Await and Actors/1. GettingStartedAsyncAwait/GettingStartedAsyncAwait/CurrentDateListViewModel.swift
+++ Async:Await and Actors/1. GettingStartedAsyncAwait/GettingStartedAsyncAwait/CurrentDateListViewModel.swift
@@ -27,6 +27,13 @@ class CurrentDateListViewModel: ObservableObject {
     
 }
 
+/**
+- fungsi async tidak bisa di panggil dengan cara biasa namun harus menggunakan try await
+- dikarnakan untuk memanggil core function get date adalah fungsi async dan untuk memanggil itu di view model ada await maka fungsi ini harus di tambahkan async
+- gunakan try await Webservice().getDate() untuk dapat menggunakan try catch
+ - 
+ */
+
 struct CurrentDateViewModel {
     let currentDate: CurrentDate
     
