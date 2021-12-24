4af8e21bac42a4251f42f3bc84b498a82708a69b
Nur Irfan Pangestu <80233829+iosdevaltea@users.noreply.github.com>
2021-12-24 23:18:59 +0700

    + add 4 project

---
+++ Async:Await and Actors/4. NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Mohammad Azam on 6/30/21.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NewsSourceListScreen() 
+        }
+    }
+}
+/**
+ 
+ */
